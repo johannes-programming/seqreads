17923f9680b1b7de6c7b71bea2a2b66386f400bf
Johannes <johannes-programming@mailfence.com>
2024-09-11 20:04:24 +0200

    Version 0.1.2

--- src/seqreads/__init__.py
+++ src/seqreads/__init__.py
@@ -7,8 +7,9 @@ import na_quantors as _na
 
 @_dataclasses.dataclass(frozen=True)
 class SeqRead:
-    qualities:float
-    seq:_Seq.Seq
+    qualities: float
+    seq: _Seq.Seq
+
     def __post_init__(self, *args, **kwargs):
         cls = type(self)
         for k, v in __annotations__.items():
@@ -17,18 +18,21 @@ class SeqRead:
                 raise TypeError
         if len(self) and _na.notna(self.qualities):
             raise ValueError
+
     @classmethod
-    def by_seq_and_qualities(cls, seq="", qualities='nan'):
+    def by_seq_and_qualities(cls, seq="", qualities="nan"):
         seq = _Seq.Seq(rec.seq)
         qualities = float(qualities)
         return cls(seq=seq, qualities=qualities)
+
     @classmethod
     def by_seqRecord(cls, rec):
         try:
-            qualities = float(sum(rec.letter_annotations['phred_quality']))
+            qualities = float(sum(rec.letter_annotations["phred_quality"]))
         except:
-            qualities = 'nan'
+            qualities = "nan"
         return cls.by_seq_and_qualities(seq=rec.seq, qualities=qualities)
+
     @classmethod
     def by_seqReads(cls, *reads):
         ans = cls.by_seq_and_qualities("", 0)
@@ -38,6 +42,7 @@ class SeqRead:
             if len(read):
                 ans += read
         return ans
+
     def to_record(self):
         rec = _SeqRecord.SeqRecord(self.seq)
         if len(self) == 0:
@@ -47,27 +52,32 @@ class SeqRead:
         except:
             pass
         else:
-            rec.letter_annotations['phred_quality'] = [q] * len(self)
+            rec.letter_annotations["phred_quality"] = [q] * len(self)
         return rec
+
     def __len__(self):
         return len(self.seq)
+
     def __add__(self, other):
         return self.by_seqReads(self, other)
+
     def __mul__(self, other):
         cls = type(self)
         return cls(
             self.seq * other,
             self.qualities * other,
         )
+
     def __rmul__(self, other):
         cls = type(self)
         return cls(
             other * self.seq,
             other * self.qualities,
         )
+
     @property
     def qv(self):
         try:
             return self.qualities / len(self)
         except ZeroDivisionError:
-            return float('nan')
+            return float("nan")
