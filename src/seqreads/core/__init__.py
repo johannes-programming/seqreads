d747dc2db129cee07cffc58e945368c874502bde
Johannes <johannes-programming@mailfence.com>
2025-02-08 23:07:36 +0100

    Version 0.1.2+local.10

--- src/seqreads/core/__init__.py
+++ src/seqreads/core/__init__.py
@@ -6,6 +6,7 @@ import na_quantors as _na
 
 __all__ = ["SeqRead"]
 
+
 @_dataclasses.dataclass(frozen=True)
 class SeqRead:
     qualities: float
@@ -13,7 +14,7 @@ class SeqRead:
 
     def __add__(self, other):
         return self.by_seqReads(self, other)
-    
+
     def __len__(self):
         return len(self.seq)
 
@@ -26,33 +27,21 @@ class SeqRead:
 
     def __post_init__(self, *args, **kwargs):
         cls = type(self)
-        for k, v in __annotations__.items():
+        for k, v in cls.__annotations__.items():
             attr = getattr(self, k)
             if type(attr) is not v:
                 raise TypeError
         if len(self) and _na.notna(self.qualities):
             raise ValueError
-        
+
     def __rmul__(self, other):
-        cls = type(self)
-        return cls(
-            other * self.seq,
-            other * self.qualities,
-        )
+        return self * other
 
     @classmethod
     def by_seq_and_qualities(cls, seq="", qualities="nan"):
-        seq = _Seq.Seq(rec.seq)
+        seq = _Seq.Seq(seq)
         qualities = float(qualities)
         return cls(seq=seq, qualities=qualities)
-
-    @classmethod
-    def by_seqRecord(cls, rec):
-        try:
-            qualities = float(sum(rec.letter_annotations["phred_quality"]))
-        except:
-            qualities = "nan"
-        return cls.by_seq_and_qualities(seq=rec.seq, qualities=qualities)
 
     @classmethod
     def by_seqReads(cls, *reads):
@@ -63,6 +52,14 @@ class SeqRead:
             if len(read):
                 ans += read
         return ans
+
+    @classmethod
+    def by_seqRecord(cls, rec):
+        try:
+            qualities = float(sum(rec.letter_annotations["phred_quality"]))
+        except:
+            qualities = "nan"
+        return cls.by_seq_and_qualities(seq=rec.seq, qualities=qualities)
 
     @property
     def qv(self):
--- src/seqreads/tests/__init__.py
+++ src/seqreads/tests/__init__.py
@@ -5,7 +5,7 @@ __all__ = ["test"]
 
 def test():
     loader = unittest.TestLoader()
-    tests = loader.discover(start_dir="datahold.tests")
+    tests = loader.discover(start_dir="seqreads.tests")
     runner = unittest.TextTestRunner()
     result = runner.run(tests)
     return result
